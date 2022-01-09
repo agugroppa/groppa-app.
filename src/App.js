4a3c62133ed7b4ec00ba07ca67bbcf29ec6c3bd4
Agustin Groppa <agustingroppa@gmail.com>
2022-01-08 21:09:49 -0300

    NavBar
    Se agrega NavBar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import { Nav } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HOLAA REACT JS!!.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <NavBar/>
       </header>
     </div>
   );
